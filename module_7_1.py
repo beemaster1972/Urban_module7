044184b06fe0249d13c1af7c0f4b6f0c2b5eb502
Дмитрий Янно <ugor.finno@yandex.ru>
2024-12-12 15:46:42 +0500

    modify module 7 hw 1 File Opening modes

--- module_7_1.py
+++ module_7_1.py
@@ -22,7 +22,7 @@ class Shop:
     def add(self, *products: Product) -> None:
         list_products = self.get_products()
         for product in products:
-            if str(product).split()[0] in list_products:
+            if str(product) in list_products:
                 print(f'{product} уже есть в магазине')
                 continue
             list_products += (str(product) + '\n')
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     s1 = Shop()
     p1 = Product('Potato', 50.5, 'Vegetables')
     p2 = Product('Spaghetti', 3.4, 'Groceries')
-    p3 = Product('Carrot', 5.5, 'Vegetables')
+    p3 = Product('Potato', 5.5, 'Vegetables')
     print(p2)  # __str__
     s1.add(p1, p2, p3)
     print(s1.get_products())
